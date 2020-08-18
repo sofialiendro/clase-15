a18d402ca81df9c8efd734853a29e4f43770ab74
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-18 20:06:02 -0300

    Agrego ejercicios

---
+++ index.js
@@ -0,0 +1,12 @@
+// Ejercicio 17
+
+let obtenerCompetencia = (a, b) => {
+    return `${a} vs. ${b}` 
+}
+
+console.log(obtenerCompetencia("Python", "JavaScript"))
+console.log(obtenerCompetencia("Coca Cola", "Pepsi"))
+
+
+// Ejercicio 18
+
