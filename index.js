45e174c7061cc83e8c66b5383fdb3783e296d422
Sofia Liendro <sofialiendro.s@gmail.com>
2020-08-18 20:19:27 -0300

    Agrego ejercicio JS

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ console.log(obtenerCompetencia("Coca Cola", "Pepsi"))
 
 // Ejercicio 18
 
+let generarEmail = (email, dominio) => {
+    return `${email}@${dominio}`
+}
+
+console.log(generarEmail("sofialiendro", "gmail.com"))
+
+
+// Ejercicio 19
+
+let calcularPuntaje = (facil, normal, dificil) => {
+    let cantidadFaciles = facil * 3
+    let cantidadNormales = normal * 5
+    let cantidadDificiles = dificil * 10
+    return cantidadFaciles + cantidadNormales + cantidadDificiles
+}
+
+console.log(calcularPuntaje(3, 0, 0))
+console.log(calcularPuntaje(0, 2, 1))
+console.log(calcularPuntaje(5, 1, 2))
